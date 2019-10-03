6f6db20bea5be00de843f7268ed3e132c4c2797a
nstazusa <33049994+nstazusa@users.noreply.github.com>
2019-10-03 10:18:32 +0800

    Typo fixing and update for the API limited update

--- mt4_geo_report.py
+++ mt4_geo_report.py
@@ -21,7 +21,7 @@ dataDir=".\\MT4_Server_Log\\47.88.153.124"
 rptDir=".\\report"
 raw_data=[]
 waitSec = 60
-waitRow = 145
+waitRow = 45
 autoBool = 0
 inputStr = ""
 
@@ -40,22 +40,22 @@ def printWaitingMsg(minutesCount):
 
 # take 2 int as intput, formant and print the progress status in %
 def printWaitingMsgRate(progCount, finCount):
-    
+
     cRate = round((progCount / finCount * 100.0),1)
     #print ("[INFO] Porgress of the data processing rate is about %s%% ..." % (cRate))
     print ("[INFO] Porgress of the data processing rate is about %s%%  (%s/%s)..." % (cRate, progCount, finCount))
-    
+
 
 
 # Digest the MT4 app log, then return / append an array of the data for the reports
 # Accepting below variables : logPath,
 def generateRawList(logPath):
 
-    # validate the target data exists in the data dir 
-    if len(glob.glob(logPath)) == 0: 
+    # validate the target data exists in the data dir
+    if len(glob.glob(logPath)) == 0:
         print ("[ERROR] *****The data in "+logPath+" is not available, the script exits... !!!!!*****")
         sys.exit( )
-        
+
 
     #Get the line count for progress estaimation
     num_lines = 0
@@ -107,14 +107,14 @@ def generateRawList(logPath):
                     # For Debug only
                     #raw_data.append([l[2], l[9], l[15]])
                     #print (raw_data[-1])
-                    #printWaitingMsgRate(len(raw_data), expectedCount)                    
+                    #printWaitingMsgRate(len(raw_data), expectedCount)
 
                 if (countRow % waitRow) == 0:
                     countRow = 0
-                    printWaitingMsgRate(len(raw_data), expectedCount)                    
+                    printWaitingMsgRate(len(raw_data), expectedCount)
                     sleep(waitSec)
-    
-    printWaitingMsgRate(len(raw_data), expectedCount)                    
+
+    printWaitingMsgRate(len(raw_data), expectedCount)
 
 
 
@@ -126,7 +126,7 @@ def genHighestLatencyRpt(num):
     #remove duplicated record
     myList = []
     tempList = sorted(raw_data,key=lambda l:int(l[3]), reverse=True)
-    
+
     expCount = len(tempList)
     for i in range(0,expCount):
         if i is 0: myList.append(tempList[i])
@@ -138,14 +138,14 @@ def genHighestLatencyRpt(num):
         num = len(myList)
 
     print ("[INFO] Starting to generate the hlRptPath ...")
-    
+
     tString = "IP, Region, Data Center, Ping Latency(ms)\n"
     f= open(hlRptPath,"wb")
     f.write(tString.encode('utf-8-sig'))
 
     lineCount = 0
     for index in range(0,len(myList)):
-        
+
         tString = (','.join(str(item) for item in myList[index]))+"\n"
         f.write(tString.encode('utf-8-sig'))
 
@@ -173,9 +173,9 @@ def genGeoLocationRpt():
     # retrive the IP & location data & sort the array
     expCount = len(myList)
     for i in range(0, expCount):
-       
+
         if (i / expCount) * 100 % 10 == 0: printWaitingMsgRate(i, expCount)
-        
+
         if len(buffList) != 0:
             if buffList[-1] != [myList[i][0], myList[i][1]]:
                 buffList.append([myList[i][0], myList[i][1]])
@@ -183,11 +183,11 @@ def genGeoLocationRpt():
             buffList.append([myList[i][0], myList[i][1]])
 
 
-    print ("[INFO] The raw data arrway has been stroed ...")
+    print ("[INFO] The raw data array has been stroed ...")
 
     #Generate the Geo distribution report
     print ("[INFO] Starting to generate the "+geoRpPath+" ...")
-    
+
     tString = "Region, Hit Count\n"
     f= open(geoRpPath,"wb")
     f.write(tString.encode('utf-8-sig'))
